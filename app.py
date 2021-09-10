f8c150631812e35438455a3af944f16e447d32a4
Marc Card <marcacard@gmail.com>
2021-09-09 20:51:21 -0400

    add 404 functionality

--- app.py
+++ app.py
@@ -23,6 +23,11 @@ def index():
     return redirect("/users")
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+
 #####################
 # === User Routes ===
 
